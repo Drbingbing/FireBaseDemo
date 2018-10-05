fc8464fb91aaabbd030f75d69cefe12de90cadab
Dr BingBing <tw718140@gmail.com>
2018-10-05 15:02:01 +0800

    Initial Commit

---
+++ FireBaseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseDemo
+//
+//  Created by Dr. BingBing on 2018/10/5.
+//  Copyright © 2018 Dr. BingBing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
