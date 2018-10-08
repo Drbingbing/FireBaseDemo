ffe6073390deb6f6f6f325423fecc7d5c902830b
Dr BingBing <tw718140@gmail.com>
2018-10-08 19:38:16 +0800

    重設密碼功能的新增

--- FireBaseDemo/Controller/ResetPaaswordViewController.swift
+++ FireBaseDemo/Controller/ResetPaaswordViewController.swift
@@ -7,8 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class ResetPaaswordViewController: UIViewController {
+    
+    @IBAction func resetPassword(sender: UIButton) {
+        
+        // Validate the input
+        guard let emailAddress = emailTextField.text, emailAddress != "" else {
+            let alertController = UIAlertController(title: "Input Error", message: "Please provide your email address for password reset", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        // Send password reset email
+        Auth.auth().sendPasswordReset(withEmail: emailAddress, completion: {(error) in
+            
+            let title = (error == nil) ? "Password Reset Follow-up" : "Password Reset Error"
+            let message = (error == nil) ? "We have just sent you a password reset email. Please check your index box and follow the instructions to reset your password" : error?.localizedDescription
+            
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: {(action) in
+                
+                if error == nil{
+                    // dismiss keyboard
+                    self.view.endEditing(true)
+                    
+                    // return to the login screen
+                    if let navcontroller = self.navigationController {
+                        navcontroller.popViewController(animated: true)
+                    }
+                }
+            })
+            alertController.addAction(okayAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        })
+    }
     
     @IBOutlet var emailTextField: UITextField!
 
